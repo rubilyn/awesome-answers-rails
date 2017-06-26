6f93f4225eb7f88b71ff293956d273b09b954f69
Rubilyn <rubilyn@gmail.com>
2017-06-26 15:45:21 -0700

    add deleting of likes via AJAX

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,18 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  def create
-    question = Question.find(params[:question_id])
-    like = Like.new(question: question, user: current_user)
+  before_action :find_question
 
-    if cannot? :like, question
-      flash[:alert] = "Cannot like your own question, dummy!"
-    elsif like.save
-      flash[:notice] = "Thanks for liking!"
+  def create
+    @like = Like.new(question: @question, user: current_user)
+
+    if cannot? :like, @question
+      like_flash :alert, "Cannot like your own question, dummy!"
+    elsif @like.save
+      like_flash :notice, "Thanks for liking!"
     else
-      flash[:alert] = like.pretty_errors
+      like_flash :alert, @like.pretty_errors
     end
 
-    @question = question
     respond_to do |format|
       format.html { redirect_to question_path(question) }
       format.js { render }
@@ -27,7 +27,23 @@ class LikesController < ApplicationController
     else
       flash[:alert] = like.pretty_errors
     end
+    respond_to do |format|
+      format.html { redirect_to question_path(like.question) }
+      format.js { render }
+    end
+  end
 
-    redirect_to question_path(like.question)
+  private
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def like_flash(type, message)
+    # like_flash(:alert, "Failed to save")
+    if request.format.js?
+      flash.now[type] = message
+    else
+      flash[type] = message
+    end
   end
 end
